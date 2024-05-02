f82a5eb5e5aa91d1c130da4581ff8554c8c6fbdb
ferminarpone <arponefermin@gmail.com>
2024-05-02 17:49:20 -0300

    Update PORT

--- Backend+Frontend-Handlebars/src/config/config.js
+++ Backend+Frontend-Handlebars/src/config/config.js
@@ -15,7 +15,7 @@ dotenv.config({
 });
 
 export default {
-  port: process.env.PORT || 9090,
+  port: process.env.PORT || 8080,
   mongo_url: process.env.MONGO_URL,
   password: process.env.PASSWORD,
   gmailAccount: process.env.GMAIL_ACCOUNT,
